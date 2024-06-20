8d3e376266f3d54ec6a6ea2c072be0888732e2d9
rudargeneira80 <rudargeneira80@gmail.com>
2024-06-20 18:51:15 +0000

    cambio de app controller y app service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,4 +1,14 @@
-/* eslint-disable prettier/prettier */
+// /* eslint-disable prettier/prettier */
+// import { Injectable } from '@nestjs/common';
+
+// @Injectable()
+// export class AppService {
+//   getHello(): string {
+//     return 'Hello World!';
+//   }
+// }
+
+
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
